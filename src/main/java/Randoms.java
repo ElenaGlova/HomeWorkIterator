962e66c45119e1a540bf0c421e0e739e37fb46e3
ElenaGolovataya <golovataya.e@yandex.ru>
2022-10-20 13:18:46 +0300

    First commit

---
+++ src/main/java/Randoms.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Random;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+public class Randoms implements Iterable<Integer> {
+
+    protected Random random;
+    protected int[] randomMass;
+
+    public Randoms(int min, int max) {
+        random = new Random();
+        randomMass = new int[max - min + 1];
+        for (int i = 0; i < randomMass.length; i++) {
+            if (i == 0) {
+                randomMass[i] = min;
+            } else {
+                randomMass[i] = randomMass[i - 1] + 1;
+            }
+        }
+    }
+
+    @Override
+    public Iterator<Integer> iterator() {
+        return new Iterator() {
+            int index;
+
+            @Override
+            public boolean hasNext() {
+                return index < randomMass.length;
+            }
+
+            @Override
+            public Integer next() {
+                int randomIndex = random.nextInt(randomMass.length);
+                return randomMass[randomIndex];
+            }
+        };
+    }
+}
